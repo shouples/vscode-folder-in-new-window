3c7715addf4ee83e8c69c8a00634707ec22c0f0e
Dave Shoup <dave.shoup@gmail.com>
2025-05-06 09:04:42 -0400

    fix test config

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,9 @@
-import { defineConfig } from '@vscode/test-cli';
+import { defineConfig } from "@vscode/test-cli";
 
 export default defineConfig({
-	files: 'out/test/**/*.test.js',
+  files: "out/*.test.js",
+  mocha: {
+    ui: "bdd",
+    timeout: 10_000,
+  },
 });
